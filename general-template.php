2bb91bcf22a5118f37c29147bb9aef5765941e48
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-09-24 18:52:53 -0300

    Templates de páginas (parte 2)

--- general-template.php
+++ general-template.php
@@ -1,13 +1,17 @@
-<!--Fizemos um template para a página sobre, para esse arquivo ser chamado devemos colocar page-slug da página, estamos na página sobre que tem o slug sobre, logo o nome da página precisa ser page-sobre.php.
-Também é possível termos um template que serve para mais de uma página-->
+<?php 
+/*
+Template Name: General Template
+*/
+?>
+<!--Template que serve para várias páginas, para aplicar nas páginas devemos ir na administração do WordPress, escolher a página e selecionar o modelo de template e salvar
+Esse nome que demos a esse template vai aparecer na administração do WordPress-->
 <?php get_header(); ?>
 	<div class="content-area">
 		<main>
 			<section class="middle-area">
 				<div class="container">
-					<div class="row">
-						<aside class="sidebar col-md-4">Barra Lateral</aside>
-						<div class="news col-md-8">
+
+						<div class="general-template">
 							<?php 
 							// Se houver algum post
 							if( have_posts() ):
@@ -18,9 +22,6 @@ Também é possível termos um template que serve para mais de uma página-->
 
 							<article>
 								<h2><?php the_title(); ?></h2>
-								<p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-								<p>Categories: <?php the_category( ' ' ); ?></p>
-								<p><?php the_tags( 'Tags: ', ', ' ); ?></p>
 								<?php the_content(); ?>
 							</article>
 
@@ -28,13 +29,12 @@ Também é possível termos um template que serve para mais de uma página-->
 								endwhile;
 							else:
 							?>
-
 							 <p>There's nothing yet to be displayed...</p>
 
 							<?php endif; ?>
 
 						</div>						
-					</div>
+
 				</div>				
 			</section>
 		</main>
