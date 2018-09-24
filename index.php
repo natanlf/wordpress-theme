4f0672cf3d14ced534e02d9e6df1dec8169bbe05
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-09-24 11:28:54 -0300

    Os três blocos básicos de um tema (parte 1)

--- index.php
+++ index.php
@@ -4,8 +4,29 @@
 	<meta charset="UTF-8">
 	<title>Curso WordPress</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
 	<p>Este é um parágrafo de referência.</p>
-	
+	<header>
+		<section class="top-bar">
+			<div class="social-media-icons"></div>
+			<div class="search"></div>
+		</section>
+		<section class="menu-area">
+			<section class="logo"></section>
+			<nav class="menu"></nav>
+		</section>
+	</header>
+	<div class="content-area">
+		<main>
+			<section class="slide"></section>
+			<section class="services"></section>
+			<section class="middle-area">
+				<aside class="sidebar"></aside>
+				<div class="news"></div>
+			</section>
+			<section class="map"></section>
+		</main>
+	</div>
+	<footer></footer>
 </body>
 </html>
