ef3d989ee51069696f54ab311e51f0f6dc7278cf
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-09-25 18:32:33 -0300

    Template parts

--- index.php
+++ index.php
@@ -17,15 +17,8 @@
 								while( have_posts() ): the_post();
 
 							?>
-							<!-- Chamamos os métodos correspondentes e nas categorias comocamos como parametro um espaço pois pode ter algum post que pertence a mais de uma categoria, assim funciona como um separador. Em Tags usamos a vírgula como separador -->
-							<article>
-								<h2><?php the_title(); ?></h2>
-								<?php the_post_thumbnail(array(275, 275));  ?>
-								<p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-								<p>Categories: <?php the_category( ' ' ); ?></p>
-								<p><?php the_tags( 'Tags: ', ', ' ); ?></p>
-								<?php the_content(); ?>
-							</article>
+							
+							<?php get_template_part( 'template-parts/content' ); ?>
 
 							<?php 
 								endwhile;
