e3e90be2541833db3e50676c81d283c41278b212
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-10-26 20:10:30 -0300

    Chamando o widget da barra lateral na pagina do blog

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
-	<?php get_header(); ?>
+<?php get_header(); ?>
 	<!-- Chama nosso cabeçalho customizado. A classe img-fluid faz a imagem ficar responsiva -->
 <img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 	<div class="content-area">
-		<main>		
+		<main>
+			<section class="slide">Slide</section>
+			<section class="services">Serviços</section>
 			<section class="middle-area">
 				<div class="container">
 					<div class="row">
-						
+					<?php get_sidebar( 'blog' ); ?>
 						<div class="news col-md-8">
 								<?php 
 							// Se houver algum post
 							if( have_posts() ):
 								// Enquanto houver posts, mostre-os pra gente
 								while( have_posts() ): the_post();
-
 							?>
 							<!--Aqui chamamos o template da página. Chamamos o content e se tiver um formato de image ou video, chamaremos o content-image ou content-vide.
 								Isso depende do formato do post, se tiver um formato padrão então chama o template content, caso seja imagem o content-image, pois esse método get_post_format(), imprime na tela o formato do post, assim podemos concatenar a chamada do template, se o formato for image fica assim: content-image-->
@@ -21,30 +22,17 @@
 
 							<?php 
 								endwhile;
-								?>
-								<!-- Paginação dos posts, previous mostra os posts mais novos e next os mais antigos, pois o wordpress sempre coloca no início os mais recentes -->
-								<div class="row">
-									<div class="pages text-left col-6">
-										<?php previous_posts_link( __( "<< Newer posts", 'wpwordpress' ) ); ?>
-									</div>
-									<div class="pages text-right col-6">
-										 <?php next_posts_link( __( "Older posts >>", 'wpwordpress' ) ); ?>
-									</div>
-								</div>
-								<?php
 							else:
 							?>
 
-							 <p><?php _e( 'There&rsquo;s nothing yet to be displayed...', 'wpwordpress' ); ?></p>
+							 <p>There's nothing yet to be displayed...</p>
 
 							<?php endif; ?>
 						</div>
-						<!-- chama o widget -->
-						<?php get_sidebar( 'blog' ); ?>	
 					</div>
 				</div>			
 			</section>
+			<section class="map">Mapa</section>
 		</main>
 	</div>
 	<?php get_footer(); ?>
-	
